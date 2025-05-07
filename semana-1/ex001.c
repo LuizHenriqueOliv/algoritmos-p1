e8a10cdc7421e04df795567b36d69258c153a8af
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-07 11:45:37 -0300

    exercício 1 - lista da semana - 1

--- semana-1/ex001.c
+++ semana-1/ex001.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-    
-}
+    int m, n; 
+    printf("Informe o numero inteiro N e M para saber se N eh multiplo de M: ");
+    scanf("%d%d", &n, &m);
+
+    if (n % m == 0)
+    {
+        printf("eh multiplo!");
+    }
+    else
+    {
+        printf("nao eh multiplo!");
+    }
+    return 0;
+}   
